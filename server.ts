04ff4a1a923537a79d07ea27615e1b396bc7e768
EstebanBraun <guinitaearlstephen@gmail.com>
2026-05-20 11:03:13 +0800

    update CORS in server.ts

--- server.ts
+++ server.ts
@@ -12,7 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || 'https://ipt-2026-frontend-sjr8.onrender.com',
+  credentials: true
+}));
 
 app.use('/accounts', accountsController);
 
