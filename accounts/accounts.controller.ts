d68696bcf51e4f8ba8dc9cefbf5e7b0713246eb0
EstebanBraun <guinitaearlstephen@gmail.com>
2026-05-21 21:24:08 +0800

    added cookie options in accounts controller

--- accounts/accounts.controller.ts
+++ accounts/accounts.controller.ts
@@ -223,9 +223,9 @@ function _delete(req: any, res: any, next: any) {
 function setTokenCookie(res: any, token: any) {
     const cookieOptions = {
         httpOnly: true,
-        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        secure: true,
         sameSite: 'none' as const,
-        secure: true
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
     };
     res.cookie('refreshToken', token, cookieOptions);
 }
